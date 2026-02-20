fd8771e2fa7080487e169bb85cfd3e82c53ac28d
Grace Mcfadden <grace.m.mcfadden@gmail.com>
2026-02-19 23:31:53 -0500

    playing around

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,69 @@
 import { useState } from 'react'
+import {Table, Button} from "@mantine/core"
 import './App.css'
 
+
+const elements = [
+  { position: 6, mass: 12.011, symbol: 'C', name: 'Carbon' },
+  { position: 7, mass: 14.007, symbol: 'N', name: 'Nitrogen' },
+  { position: 39, mass: 88.906, symbol: 'Y', name: 'Yttrium' },
+  { position: 56, mass: 137.33, symbol: 'Ba', name: 'Barium' },
+  { position: 58, mass: 140.12, symbol: 'Ce', name: 'Cerium' },
+];
+
+elements.push({position: 8, mass:23.394, symbol: 'D', name: 'Dylan'})
+
+const results = [
+  {candidate: "Leo Williams", votes: 26402, percent: 0.5755},
+  {candidate: "Anjanee Bell", votes: 19290, percent: 0.4205}
+]
+
+
+function Demo() {
+  const rows = elements.map((element) => (
+    <Table.Tr key={element.name}>
+      <Table.Td>{element.position}</Table.Td>
+      <Table.Td>{element.name}</Table.Td>
+      <Table.Td>{element.symbol}</Table.Td>
+      <Table.Td>{element.mass}</Table.Td>
+    </Table.Tr>
+  ));
+
+  return (
+    <Table>
+      <Table.Thead>
+        <Table.Tr>
+          <Table.Th>Element position</Table.Th>
+          <Table.Th>Element name</Table.Th>
+          <Table.Th>Symbol</Table.Th>
+          <Table.Th>Atomic mass</Table.Th>
+        </Table.Tr>
+      </Table.Thead>
+      <Table.Tbody>{rows}</Table.Tbody>
+    </Table>
+  );
+}
+
+function resultsTable(race) {
+  const rows = race.map((res) => (
+    <Table.Tr key={res.candidate}>
+      <Table.Td>{res.votes}</Table.TD>
+      <Table.Td>{res.percent}</Table.Td>
+    </Table.Tr>
+  ));
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
+  const [count, setCount] = useState("potato")
 
   return (
     <>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <Button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -19,6 +72,7 @@ function App() {
       <p className="read-the-docs">
         Click on nothing to learn more 🫛
       </p>
+      <Demo />
     </>
   )
 }
