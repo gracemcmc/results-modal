ef7c9e9ac6538f94b604d7892a423cecd11c3f87
Grace Mcfadden <grace.m.mcfadden@gmail.com>
2026-02-25 21:08:06 -0500

    deleted old code

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,6 @@ import {GetResultsNCSBE} from './functions.ts'
 import {ResultsTable} from './Components.tsx';
 
 
-//console.log(document.referrer);
-
-let results = [
-  {candidate: "Leo Williams", votes: 26402, percent: 0.5755},
-  {candidate: "Anjanee Bell", votes: 19290, percent: 0.4205}
-];
-
-
 const urls_by_locale = [
   {outlet: "guilford", url: "https://er.ncsbe.gov/enr/20260303/data/results_41.txt?v=22-15-12"},
   {outlet: "wake", url: "https://er.ncsbe.gov/enr/20260303/data/results_92.txt?v=22-15-12"},
@@ -24,15 +16,7 @@ const urls_by_locale = [
   {outlet: "state", url: "https://er.ncsbe.gov/enr/20260303/data/results_1.txt?v=22-15-12"},
 ]
 
-/*
-const urlParams = new URLSearchParams(window.location.search);
-const form = document.getElementById('subscribe');
 
-
-urlParams.set('order', 'date');
-
-window.location.search = urlParams;
-*/
 
 // utils
 
@@ -72,101 +56,15 @@ function PlainResultsTable({race}:{race:unknown[]}) {
     );
 }
 
-/*let html_string= "content";
-let myIframe = document.getElementById('output_iframe1');
-myIframe.src = `data:text/html;charset=utf-8,${html_string}`;
-
-<iframe id="output_iframe1"></iframe>
-
-const full_results = await GetResultsNCSBE(urls_by_locale[0].url);
-let filtered = full_results.filter(item => item.lid === "2119");
-
-results = full_results;
-
-
-
-window.addEventListener("DOMContentLoaded", (event) => {
-  ncsbeurl = document.getElementById("form").url.value;
-  document.getElementById("getter").addEventListener("click", myFunction);
-  document.getElementById("demo").innerHTML = ncsbeurl;
-});
-*/
 
 export function App() {
-  // const [count, setCount] = useState(0) //action="/post" method="post" target="_self" type="url"
-  // <button onClick={() => getURL("french")}>here it is</button>
+
   let input_url = "https://er.ncsbe.gov/enr/20260303/data/results_58.txt?v=22-15-12";
+
   const [options, setOptions] = useState();
   const [ncsbeurl, getURL] = useState(" ");
   let sample = ["faber", "2147", "https://er.ncsbe.gov/enr/20260303/data/results_1.txt?v=22-15-12"];
-/*
-  function Tryingoutform() {
-
-    function handleChange(i) {
-      getURL(i.target.value);
-      createOptions(i.target.value).then(
-        result => setOptions(result));
-    }
-
-    return (
-        <form>
-          <label>add url</label>
-
-          <input type="url" value={ncsbeurl} onChange={handleChange}/> 
-          <p>Curernt {ncsbeurl}</p>
-        </form>
-      )}
-
-  async function GetResultsNCSBE(url) {
-    try {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error('Response status: {$response.status}');
-      }
-      const result = await response.json();
-      return result;
-    } catch (e: any) {
-      console.error(e.message, url);
-    };
-
-  function ResultsTable({race}:{race:unknown[]}) {
-    const rows = race.map((res_rows: any) => (
-      <Table.Tr key={res_rows.candidate}>
-        <Table.Td>{res_rows.candidate}</Table.Td>
-        <Table.Td>{res_rows.votes}</Table.Td>
-        <Table.Td>{res_rows.percent}</Table.Td>
-      </Table.Tr>
-      return (
-      <Table>
-        <Table.Thead>
-          <Table.Tr>
-          <Table.Th>Candidate</Table.Th>
-          <Table.Th>Votes</Table.Th>
-          <Table.Th>Percent</Table.Th>
-          </Table.Tr>
-        </Table.Thead>
-        <Table.Tbody>{rows}</Table.Tbody>
-      </Table>
-      );
-
-    ));}
-  }*/
-
-  async function createOptions(ncsbeurl: any) {
-    const full_results = await GetResultsNCSBE(input_url);
-    const beetle = get_uniques(full_results);
-    //let filtered = full_results.filter(item => item.lid === "2119")
-    return beetle}
-
-  function OptionsPrint(ops: any) {
-    const ps = ops.map((o: any) => (
-      <p>o</p>
-      ));
-    return ps;
-    }
-
-
-    const demo_url = urls_by_locale[1].url;
+  
     return (
       <>
         <div id="list">
